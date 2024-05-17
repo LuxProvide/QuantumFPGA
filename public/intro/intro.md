ea86d8630be82a0620e8709f375cfda2ca444b8a
Emmanuel Kieffer <emmanuel.kieffer@lxp.lu>
2024-05-17 11:08:46 +0200

    Add git-lfs to track fpga images

--- public/intro/intro.md
+++
@@ -1,1 +0,0 @@
-[![](./images/01-Introduction to FPGA computing for the HPC ecosystem.png)](./pdfs/01-Introduction to FPGA computing for the HPC ecosystem.pdf)
--- public/javascripts/tables.js
+++
@@ -1,8 +0,0 @@
-// See https://squidfunk.github.io/mkdocs-material/reference/data-tables/#sortable-tables
-
-app.document$.subscribe(function() {
-  var tables = document.querySelectorAll("article table")
-  tables.forEach(function(table) {
-    new Tablesort(table)
-  })
-})
